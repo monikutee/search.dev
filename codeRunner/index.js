23d23ebf52ec3f347f58dce506c662d7f0cf1b31
monikutee <petrulevicmonika@gmail.com>
2024-04-25 23:02:11 +0300

    add codeRunner

---
+++ codeRunner/index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const Docker = require("dockerode");
+const bodyParser = require("body-parser");
+const docker = new Docker({ socketPath: "/var/run/docker.sock" });
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/run-code", async (req, res) => {
+  const { code, language } = req.body;
+
+  try {
+    const output = await runCodeInDocker(code, language);
+    res.send({ output });
+  } catch (error) {
+    res.status(500).send({ error: "Failed to execute code" });
+  }
+});
+
+const runCodeInDocker = async (code, language) => {
+  let image;
+  let command;
+
+  switch (language) {
+    case "python":
+      image = "python:3.9-alpine";
+      command = ["python", "-c", code];
+      break;
+    case "javascript":
+      image = "node:14-alpine";
+      command = ["node", "-e", code];
+      break;
+    default:
+      throw new Error("Unsupported language");
+  }
+
+  const container = await docker.createContainer({
+    Image: image,
+    Cmd: command,
+    Tty: false,
+    AttachStdout: true,
+    AttachStderr: true,
+  });
+
+  await container.start();
+
+  const stream = await container.attach({
+    stream: true,
+    stdout: true,
+    stderr: true,
+  });
+
+  let output = "";
+  stream.on("data", (chunk) => (output += chunk.toString()));
+  await new Promise((resolve, reject) => {
+    container.wait((err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
+  });
+
+  await container.remove();
+  return output;
+};
+
+app.listen(9090, () => {
+  console.log("Server is running on http://localhost:9090");
+});
