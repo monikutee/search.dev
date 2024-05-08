9f002f8fe39adad8b4cb1bb9c1ce2ead08bbd592
monikutee <petrulevicmonika@gmail.com>
2024-05-09 00:13:45 +0300

    add job offer title to applicants view

--- client/src/pages/ViewApplicants.tsx
+++ client/src/pages/ViewApplicants.tsx
@@ -20,19 +20,32 @@ import TextField from "@mui/material/TextField";
 import CheckIcon from "@mui/icons-material/Check";
 import ClearIcon from "@mui/icons-material/Clear";
 import Checkbox from "@mui/material/Checkbox";
+import { JobOfferApplyMiniDto } from "../api/types/jobOffer";
+import {
+  RoleText,
+  ExperienceLevelText,
+  JobTypeText,
+  RemoteText,
+} from "../helpers/constants/JobOffer";
+import TimeAgo from "javascript-time-ago";
 
 export const ViewApplicants = () => {
   const [applicants, setApplicants] = React.useState<ApplicantFullI[]>([]);
+  const [jobOffer, setJobOffer] = React.useState<JobOfferApplyMiniDto | null>(
+    null
+  );
   const { userId } = useContext(UserContext);
   const { jobOfferId } = useParams<{ jobOfferId: string }>();
   const navigate = useNavigate();
+  const timeAgo = new TimeAgo("en-US");
 
   React.useEffect(() => {
     if (jobOfferId && userId) {
       try {
-        Api.jobOffer
-          .fetchApplicants(userId, jobOfferId)
-          .then((res) => setApplicants(res.data.applicants));
+        Api.jobOffer.fetchApplicants(userId, jobOfferId).then((res) => {
+          setApplicants(res.data.applicants);
+          setJobOffer(res.data.jobOffer);
+        });
       } catch {
         console.log("error");
       }
@@ -122,6 +135,37 @@ export const ViewApplicants = () => {
               Back
             </Button>
           </Link>
+        </Grid>
+        <Grid item xs={12} md={9}>
+          {jobOffer && (
+            <>
+              <div className="d-flex align-items-center justify-content-between gap-2 mt-md-0 mt-4">
+                <Typography variant="h5" color="primary">
+                  {jobOffer.title}
+                </Typography>
+                <div>
+                  <Typography variant="body1" color="secondary" noWrap>
+                    {jobOffer?.applicantCount} applicants
+                  </Typography>
+                  {jobOffer?.updatedAt && (
+                    <Typography variant="body1" color="secondary">
+                      {timeAgo.format(new Date(jobOffer.updatedAt).getTime())}
+                    </Typography>
+                  )}
+                </div>
+              </div>
+              <div className="d-flex gap-2 flex-wrap mt-2 mb-5">
+                <Chip label={JobTypeText[jobOffer.jobType]} />
+                <Chip label={RoleText[jobOffer.role]} />
+                <Chip label={ExperienceLevelText[jobOffer.experienceLevel]} />
+                <Chip
+                  label={`${jobOffer.city}, ${jobOffer.country} (${
+                    RemoteText[jobOffer.remote]
+                  })`}
+                />
+              </div>
+            </>
+          )}
         </Grid>
         <Grid item xs={12} md={9}>
           {applicants.length > 0 ? (
